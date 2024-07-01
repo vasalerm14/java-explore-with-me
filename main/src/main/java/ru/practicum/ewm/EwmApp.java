154ee0a6bdd3903109915c1a7d9adac1f9d122a8
vasalerm <vasalerm008@gmail.com>
2024-07-01 19:04:29 +0300

    Первая версия

--- main/src/main/java/ru/practicum/ewm/EwmApp.java
+++ main/src/main/java/ru/practicum/ewm/EwmApp.java
@@ -1,12 +1,11 @@
-package ru.practicum.main;
+package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication {
-
+public class EwmApp {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(EwmApp.class);
     }
 }
