df5676329ab1b68d108e0b13d366bdf18ee8d1bf
vasalerm <vasalerm008@gmail.com>
2024-07-06 02:06:54 +0300

    Правки

--- main/src/main/java/ru/practicum/ewm/events/EventServiceInt.java
+++ main/src/main/java/ru/practicum/ewm/events/EventServiceInt.java
@@ -10,20 +10,20 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventServiceInt {
-    EventFullDto addEvent(Long userId, NewEventDto newEventDto) throws NotFoundException, ValidationException;
+    EventFullDto addEvent(Long userId, NewEventDto newEventDto);
 
-    EventFullDto updateEventByOwner(Long userId, Long eventId, UpdateEventUserRequest updateEvent) throws NotFoundException, ForbiddenException, ValidationException;
+    EventFullDto updateEventByOwner(Long userId, Long eventId, UpdateEventUserRequest updateEvent);
 
-    EventFullDto updateEventByAdmin(Long eventId, UpdateEventAdminRequest updateEvent) throws NotFoundException, ForbiddenException, ValidationException;
+    EventFullDto updateEventByAdmin(Long eventId, UpdateEventAdminRequest updateEvent);
 
     List<EventShortDto> getEventsByOwner(Long userId, Integer from, Integer size);
 
-    EventFullDto getEventByOwner(Long userId, Long eventId) throws NotFoundException;
+    EventFullDto getEventByOwner(Long userId, Long eventId);
 
-    List<EventFullDtoWithViews> getEventsByAdminParams(List<Long> users, List<String> states, List<Long> categories, LocalDateTime rangeStart, LocalDateTime rangeEnd, Integer from, Integer size) throws ValidationException;
+    List<EventFullDtoWithViews> getEventsByAdminParams(List<Long> users, List<String> states, List<Long> categories, LocalDateTime rangeStart, LocalDateTime rangeEnd, Integer from, Integer size);
 
-    List<EventShortDtoWithViews> getEvents(String text, List<Long> categories, Boolean paid, LocalDateTime rangeStart, LocalDateTime rangeEnd, Boolean onlyAvailable, String sort, Integer from, Integer size, HttpServletRequest request) throws ValidationException, NotFoundException;
+    List<EventShortDtoWithViews> getEvents(String text, List<Long> categories, Boolean paid, LocalDateTime rangeStart, LocalDateTime rangeEnd, Boolean onlyAvailable, String sort, Integer from, Integer size, HttpServletRequest request);
 
-    EventFullDtoWithViews getEventById(Long eventId, HttpServletRequest request) throws NotFoundException;
+    EventFullDtoWithViews getEventById(Long eventId, HttpServletRequest request);
 
 }
